4cde91d8e87f2cb6e77b785f9762217a14d4134b
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-10-09 18:06:21 +0530

    communication between services using API Gateway

--- cashingApp/src/main/java/com/vestaChrono/cashingApp/advices/GlobalExceptionHandler.java
+++ cashingApp/src/main/java/com/vestaChrono/cashingApp/advices/GlobalExceptionHandler.java
@@ -1,21 +1,27 @@
 package com.vestaChrono.cashingApp.advices;
 
 import com.vestaChrono.cashingApp.exception.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.StaleObjectStateException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        log.error(exception.getLocalizedMessage());
         return ResponseEntity.notFound().build();
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(RuntimeException exception) {
-        return ResponseEntity.internalServerError().build();
+    @ExceptionHandler(StaleObjectStateException.class)
+    public ResponseEntity<?> handleStaleObjectStateException(StaleObjectStateException exception) {
+        log.error(exception.getLocalizedMessage());
+        return new ResponseEntity<>("Stale data \n", HttpStatus.CONFLICT);
     }
 
 }
--- cashingApp/src/main/java/com/vestaChrono/cashingApp/repositories/SalaryAccountRepository.java
+++ cashingApp/src/main/java/com/vestaChrono/cashingApp/repositories/SalaryAccountRepository.java
@@ -1,9 +1,18 @@
 package com.vestaChrono.cashingApp.repositories;
 
 import com.vestaChrono.cashingApp.entities.SalaryAccount;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SalaryAccountRepository extends CrudRepository<SalaryAccount, Long> {
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<SalaryAccount> findById(Long id);
+
 }
