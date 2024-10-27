f3879f340938b21e90e8ad8ec4bb8874502e769e
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-10-27 10:20:35 +0530

    adding user and notification service

---
+++ learnKafka/user-service/src/main/java/com/vestaChrono/ecommerce/user_service/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.vestaChrono.ecommerce.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("/{message}")
+    public ResponseEntity<String> sendMessage (@PathVariable String message) {
+
+        kafkaTemplate.send("user-random-topic", message);
+
+        return ResponseEntity.ok("Message Queued");
+    }
+
+}
