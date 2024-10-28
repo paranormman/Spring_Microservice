e5a7b3b3d56fe27d9dde2e815ff9cd62e0b7b26d
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-10-28 07:51:29 +0530

    Week5.4 Configure Kafka with Spring Boot

---
+++ learnKafka/notification-service/src/main/java/com/vestaChrono/ecommerce/notification_service/consumer/UserKafkaConsumer.java
@@ -0,0 +1,28 @@
+package com.vestaChrono.ecommerce.notification_service.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserKafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(UserKafkaConsumer.class);
+
+    @KafkaListener(topics = "user-random-topic")
+    public void handleUserRandomTopic1(String message) {
+        log.info("handleUserRandomTopic1: {}", message);
+    }
+
+    @KafkaListener(topics = "user-random-topic")
+    public void handleUserRandomTopic2(String message) {
+        log.info("handleUserRandomTopic2: {}", message);
+    }
+
+    @KafkaListener(topics = "user-random-topic")
+    public void handleUserRandomTopic3(String message) {
+        log.info("handleUserRandomTopic3: {}", message);
+    }
+
+}
