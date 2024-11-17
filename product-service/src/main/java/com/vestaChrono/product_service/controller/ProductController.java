efc62a1320d0091314d257efe6784510563432f4
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-11-17 18:59:42 +0530

    6.4 Managing multiple docker container using docker compose

---
+++ product-service/src/main/java/com/vestaChrono/product_service/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.vestaChrono.product_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot";
+    }
+
+}
